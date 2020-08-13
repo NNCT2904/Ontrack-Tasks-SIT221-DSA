0e17537d9c7ffe77215493bfc38f0ff4e95ab465
Thanh Nguyen <nnct2000@gmail.com>
2020-08-13 14:52:16 +1000

    Optimize bubble sort
    the best case is now O(n)

--- Task3.1/Task3.1/Sorter/BubbleSort.cs
+++ Task3.1/Task3.1/Sorter/BubbleSort.cs
@@ -14,20 +14,54 @@ namespace Sorter
         {
             if (comparer == null)
                 comparer = Comparer<K>.Default;
+
+            BSort(sequence, comparer);
+        }
+
+        private void BSort<K>(K[] sequence, IComparer<K> comparer) where K : IComparable<K>
+        {
+            bool sorted;
+
+            for (int i = 0; i < sequence.Length - 1; i++)
+            {
+                //Assume the array is sorted
+                sorted = true;
+
+                //Bubble sort, also check if the array is sorted or not
+                for (int j = 0; j < sequence.Length - i - 1; j++)
+                {
+                    if (comparer.Compare(sequence[j], sequence[j + 1]) > 0)
+                    {
+                        K temp = sequence[j];
+                        sequence[j] = sequence[j + 1];
+                        sequence[j + 1] = temp;
+                        sorted = false;
+                    }
+                }
+
+                //If the array is already sorted then break.
+                if (sorted == true)
+                    break;
+
+                //Best case is linear O(n) when the array is sorted!
+            }
+        }
+
+        private void OldBSort<K>(K[] sequence, IComparer<K> comparer) where K : IComparable<K>
+        {
             for (int i = sequence.Length; i >= 1; i--)
             {
                 for (int j = 0; j < i - 1; j++)
                 {
-                    if (comparer.Compare(sequence[j], sequence[j+1]) > 0)
+                    if (comparer.Compare(sequence[j], sequence[j + 1]) > 0)
                     {
                         K temp = sequence[j];
                         sequence[j] = sequence[j + 1];
                         sequence[j + 1] = temp;
                     }
                 }
-
             }
+            //Will run O(n^2) in every case
         }
-
     }
 }
